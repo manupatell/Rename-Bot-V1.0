8868a25e4f3ae081f9dd1ae470d485e5e9db9e13
Raghu Ram <manumanhothra1618@gmail.com>
2024-06-17 10:47:40 +0530

    Update config.py
    Signed-off-by: Raghu Ram <manumanhothra1618@gmail.com>

--- config.py
+++ config.py
@@ -2,22 +2,22 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = os.environ.get("API_ID", "23032012")
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "5e47a644cc456147dbc79a24511c4dbb")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7264263787:AAE72vhNLtm8SJsQhhUXM_AjFLubtWDFSsc") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "WebXBots") 
 
-DB_NAME = os.environ.get("DB_NAME","")     
+DB_NAME = os.environ.get("DB_NAME","rename")     
 
-DB_URL = os.environ.get("DB_URL","")
+DB_URL = os.environ.get("DB_URL","mongodb+srv://Kanna:Kanna@cluster0.fhbau4i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
 FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/Rename-Bot-01-15")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6704116482').split()]
 
 PORT = os.environ.get("PORT", "8080")
